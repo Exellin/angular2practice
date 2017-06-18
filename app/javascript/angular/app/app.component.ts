17f37ed9a71fa98736ec6370a3d771162731c10d
Exellin <carnegieshawn@yahoo.ca>
2017-06-18 07:30:21 +0000

    add app css component and stylesheet_pack_tag

--- app/javascript/angular/app/app.component.ts
+++ app/javascript/angular/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import templateString from './app.component.html'
+import './app.components.scss'
 
 @Component({
   selector: 'angular',
   template: templateString
 })
-export class AppComponent {
-  name = 'Angular!';
-}
+
+export class AppComponent {}
