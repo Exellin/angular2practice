72e4ba73cba39ec39efcd773f554bd780f99087d
Exellin <carnegieshawn@yahoo.ca>
2017-06-18 17:41:44 +0000

    add media-item component and put stylings in component tag names

---
+++ app/javascript/angular/app/media-item/media-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import templateString from './media-item.component.html'
+import './media-item.component.scss'
+
+@Component({
+  selector: 'media-item',
+  template: templateString
+})
+
+export class MediaItemComponent {}
