42a9bf74af28190a51c053201a93e35523d0d07e
Exellin <carnegieshawn@yahoo.ca>
2017-06-19 16:27:59 +0000

    add favorite directive

--- app/javascript/angular/app/media-item/media-item.component.ts
+++ app/javascript/angular/app/media-item/media-item.component.ts
@@ -12,6 +12,6 @@ export class MediaItemComponent {
   @Output() delete = new EventEmitter();
   
   onDelete() {
-    this.delete.emit(this.media_item);
+    this.delete.emit(this.mediaItem);
   }
 }
