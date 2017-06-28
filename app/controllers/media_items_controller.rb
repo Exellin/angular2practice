7969c33c64f5a2fcbc2bff766f73afb88331d67a
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-06-27 18:15:06 -0600

    added show and update actions to api

--- app/controllers/media_items_controller.rb
+++ app/controllers/media_items_controller.rb
@@ -1,19 +1,27 @@
 class MediaItemsController < ApplicationController
-  
+
   def index
     unless params[:medium].blank?
       media_items = MediaItem.where(medium: params[:medium])
     else
       media_items = MediaItem.all
     end
-    render json: media_items
+    render json: media_items, status: :ok
   end
-  
+
+  def show
+    media_item = MediaItem.find(params[:id])
+    if media_item
+      render json: media_item, status: :ok
+    else
+      render json: { error: error.message }, status: :not_found
+    end
+  end
+
   def create
     media_item = MediaItem.new(media_item_params)
-    media_item.save
     if media_item.save
-      render json: media_item, status: 200
+      render json: media_item, status: :ok
     else
       errors = [];
       media_item.errors.each do |key, value|
@@ -23,14 +31,28 @@ class MediaItemsController < ApplicationController
       render json: errors, status: :unprocessable_entity
     end
   end
-  
+
+  def update
+    media_item = MediaItem.find(params[:id])
+    if media_item.update(media_item_params)
+      render json: media_item, status: :ok
+    else
+      errors = [];
+      media_item.errors.each do |key, value|
+        error = {property: key, message: value}
+        errors.push(error)
+      end
+      render json: errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     media_item = MediaItem.find(params[:id])
     media_item.destroy
   end
-  
+
   private
-  
+
   def media_item_params
     params.require(:media_item).permit(:name, :medium, :category, :year, :watchedOn, :isFavorite)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :media_items, only: [:index, :create, :destroy]
+  resources :media_items, only: [:show, :index, :create, :destroy, :update]
   get "*unmatched_route", to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
