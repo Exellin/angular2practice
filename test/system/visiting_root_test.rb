bc56f6d4f7b321d113dc85262ca028cabf3d72b0
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-07-09 14:52:34 -0600

    fix root test name

--- test/system/visiting_root_test.rb
+++ test/system/visiting_root_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class UsersTest < ApplicationSystemTestCase
+class RootSpec < ApplicationSystemTestCase
   test 'visiting root route' do
     visit '/'
     assert_text('Media Watch List')
